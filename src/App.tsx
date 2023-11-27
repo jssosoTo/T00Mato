e00391b887597518404cbce35403704c754f076f
suyuxiang-tastien <suyuxiang@tastien.com>
2023-11-27 17:47:37 +0800

    ✨ feat(全局): 路由配置以及格式化

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,45 @@
+import { Routes, Route } from 'react-router-dom';
+import { ReactElement } from 'react';
+import Error from './pages/Error';
+
+type RouteItem = {
+  path: string;
+  element: ReactElement;
+  children?: RouteItem[];
+};
+
+function formatRoutes(routeItems: RouteItem[] | []): ReactElement[] {
+  return routeItems.map(
+    (routeItem: {
+      path: string;
+      element: ReactElement;
+      children?: RouteItem[];
+    }) => {
+      return (
+        <>
+          <Route
+            key={Date.now()}
+            path={routeItem.path}
+            element={routeItem.element}
+          ></Route>
+          {routeItem.children && formatRoutes(routeItem.children || [])}
+        </>
+      );
+    }
+  );
+}
+
 function App() {
-  return <div>App</div>;
+  const routes = [];
+
+  return (
+    <div>
+      <Routes>
+        {formatRoutes(routes)}
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
